7666b793515e44c44f0b3c4027a9b9409a29c2a5
ZYN <zyn@MacBook-Pro.local>
2018-01-03 10:05:10 +0800

    - init

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by ZYN on 2017/11/1.
+//  Copyright © 2017年 bxtth.itm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
